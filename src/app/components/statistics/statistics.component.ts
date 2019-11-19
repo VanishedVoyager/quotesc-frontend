70a33e8dadaefff77f342856fb2ef10dcfe3c415
JanNeuburger <jan.neuburger.jn@gmail.com>
2019-11-19 09:48:13 +0100

    Experimented with charts

--- src/app/components/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsComponent implements OnInit {
 
+  title = 'Quotes per Person';
+  data = [
+    ['Jan Neuburger', 45.0],
+    ['Erik Mayhofer', 26.8],
+    ['Maximilian Schuster', 12.8],
+    ['Florian Schwarzc', 8.5],
+    ['Maximilian Wahl', 6.2],
+    ['Joules Zapata', 0.7]
+    ];
+  type = 'PieChart';
+  columnNames = ['Person', 'Percentage'];
+  options = {
+    is3D: false
+  };
+
   constructor() { }
 
   ngOnInit() {
